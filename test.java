20731d8194bfc909787afc5324b5905db44606f8
Richard Stewing <richy.sting@gmail.com>
2016-04-12 07:55:00 -0700

    test with classes

--- test.java
+++ test.java
@@ -5,8 +5,42 @@ import java.io.InputStreamReader;
 
 class test{
 	public static void main(String[] args){
-		System.out.println("test");
+	    Rectangle rect;
+	    rect = new Rectangle(Integer.parseInt(args[0]),
+				 Integer.parseInt(args[1]));
+	    System.out.println(rect.Area());
+	    rect.Print();
+	    System.out.println();
 	}
+}
+
+class Rectangle {
+    private int height;
+    private int width;
+
+    public Rectangle(int h, int w){
+	height = h;
+	width  = w;
+    }
+    public int Area(){
+	return height*width;
+    }
+
+    public void Print(){
+	for(int i = 0; i < width; i++)
+	    System.out.print("_");
+	System.out.println();
+	for(int n = 0; n < height; n++){
+	    System.out.print("|");
+	    for(int k = 0; k < width-1; k++)
+		System.out.print(" ");
+	    System.out.print("|");
+	    System.out.println();
+	}
+
+	for(int m = 0; m < width; m++)
+	    System.out.print("-");
+    }
 }
 
 
