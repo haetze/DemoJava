4a8d17247ed2ed8e3cb594731085aeb84fe2819d
Richard Stewing <richy.sting@gmail.com>
2016-04-20 00:24:20 -0700

    added explanatory comments

--- Vorlesung1.java
+++ Vorlesung1.java
@@ -6,7 +6,7 @@
 class Vorlesung1 {
 
     public static void main(String[] argv){
-	System.out.println(sumOfSqTo(10));
+	System.out.println(SQ(9,10));
     }
 
     public static int sumOfSqTo(int n){
@@ -14,10 +14,21 @@ class Vorlesung1 {
     }
 
 
-    public static int SQ(int lowBound, int highBound){
+    //returns sum of squares
+    //both bounds' square's are inclusive
+    public static int SQ(int lowBound, int highBound){ 
 	if(highBound <= lowBound)
 	    return (highBound*highBound);
 
 	return (highBound*highBound)+SQ(lowBound, highBound - 1);
     }
 }
+
+/*
+
+sumOfSq = sum . map (\x -> x*x)
+sumOfSqWithUpperBound x = sumOfSq [1..x]
+sumOfSqWithLowerAndUpperBounds x y = sumOfSq [x..y]
+
+the same program in Haskell
+*/
